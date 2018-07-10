bd7218908e7049a6f34cdf41ccc1a49ce5d25355
IEugeneI <eugenelboichenko@gmail.com>
2018-07-10 22:08:54 +0300

    doing logo and navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,6 @@
+<?php
+$background = 'background.png';
+?>
 <!doctype html>
 <html lang="en">
     <head>
@@ -9,23 +12,74 @@
         <link rel="stylesheet" href="../../public/css/font-awesome-4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="../../public/css/style.css">
         <style>
+            body{
 
+                background:url('../image/<?= $background ?>')   center no-repeat;
+
+
+                background-size:cover;
+
+            }
         </style>
         <title>Gloria FX studio!</title>
     </head>
     <body>
-        <div class="container-fluid">
-            <div class="row ">
 
-                <div class="col offset-lg-11 offset-xl-11 offset-md-11 d-none d-md-block media">
-
-                    <a class="fa fa-instagram fa-2x " href="https://www.instagram.com/gloriafx/" target="_blank"></a>
-                    <a class="fa fa-facebook fa-2x " href="https://www.facebook.com/gloriafx" target="_blank"></a>
-
-
+        <div class="d-md-none">
+            <div class="navbar navbar-dark bg-dark  ">
+                <div class="navbar-header">
+                    <a class="navbar-brand mylogo" href="#">
+                        <img  id="brand-image" src="../../public/image/logo.png" alt="" ></img>
+                    </a>
                 </div>
 
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#responsiveMenu" >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+
+                <div class="collapse navbar-collapse id="responsiveMenu">
+                     <ul class="nav navbar-nav">
+                        <li><a href="#">321</a></li>
+                        <li><a href="#">4324</a></li>
+                        <li><a href="#">3rwer</a></li>
+                    </ul>
+                </div>
             </div>
+            <!--  <div class="navbar navbar-dark bg-dark  ">
+                  <div class="container">
+                      <div class="navbar-header">
+                          <a class="navbar-brand" href="#">Логотип</a>
+
+                      </div>
+                      <button class="navbar-toggle " type="button" data-toggle="collapse" data-target="#menu" >
+                          <span class="navbar-toggler-icon"></span>
+                      </button>
+                      <div class="collapse navbar-collapse" id="menu">
+
+                          <ul class="nav navbar-nav">
+                              <li class="active"><a href="#">Home</a></li>
+                              <li><a href="#">rwe213</a></li>
+                              <li><a href="#">rwe421</a></li>
+                              <li><a href="#">rwe111</a></li>
+                          </ul>
+                          <ul class="nav navbar-nav navbar-right">
+                              <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                              <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>-->
         </div>
+        <div class="col offset-lg-11 offset-xl-11 offset-md-11 d-none d-md-block media">
+
+            <a class="fa fa-instagram fa-2x " href="https://www.instagram.com/gloriafx/" target="_blank"></a>
+            <a class="fa fa-facebook fa-2x " href="https://www.facebook.com/gloriafx" target="_blank"></a>
+
+
+        </div>
+
+
+
     </body>
 </html>
